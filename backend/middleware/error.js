e78a7fd84caaf4423482e03e17febfdd5a1de579
Udbhas Dutta <udbhasdutta@gmail.com>
2023-12-25 23:09:32 +0530

    added env to gitignore

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -12,8 +12,28 @@ module.exports = (err, req, res, next) => {
         err = new ErrorHandler(message, 400)
     }
 
+    //Mongoose duplicate key error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+        err = new ErrorHandler(message, 400)
+    }
+
+    //Wrong JWT token error
+    if(err.name === "JsonWebTokenEror"){
+        const message = `JSON Web Token is invalid, try again`;
+        err = new ErrorHandler(message, 400)
+    }
+
+    //JWT expire error
+    if(err.name === "TokenExpiredEror"){
+        const message = `JSON Web Token has Expired, try again`;
+        err = new ErrorHandler(message, 400)
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message
     }); 
+
+
 };
