df521e56043bf3604e5d105dfc2b5172b402c609
Udbhas Dutta <udbhasdutta@gmail.com>
2024-01-02 10:41:16 +0530

    added header, images, and started footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import { useEffect } from 'react';
 import './App.css';
+import Header from './components/layout/Header/Header.js'
+import { BrowserRouter as Router } from 'react-router-dom'
+import WebFont from "webfontloader"
+import Footer from './components/layout/Footer/Footer.js'
+
 
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google:{
+        families:["Roboto", "Droid Sans", "Chilanka"]
+      },
+    });
+  }, [])
+  
   return (
-    <h1>Hello frontend</h1>
+    <Router>
+      <Header />
+
+      <Footer/>
+    </Router>
   );
 }
 
