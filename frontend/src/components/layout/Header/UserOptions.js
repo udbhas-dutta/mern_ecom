596d661b3748eceed335e385ead1c264570c83d4
Udbhas Dutta <udbhasdutta@gmail.com>
2024-01-28 01:03:17 +0530

    added userOptions.js

--- frontend/src/components/layout/Header/UserOptions.js
+++ frontend/src/components/layout/Header/UserOptions.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
+import { SpeedDial, SpeedDialAction } from '@mui/material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import { useNavigate } from 'react-router-dom';
 
-const UserOptions = () => {
+
+const UserOptions = ({ user }) => {
+  const [open, setOpen] = useState(false)
+
+  const options = [
+    { icon: <ListAltIcon />, name: "Orders", func: orders },
+    { icon: <PersonIcon />, name: "Profile", func: account },
+    { icon: <ExitToAppIcon />, name: "Logout", func: logOutUser },
+  ]
+
+  if (user.role === "admin") {
+    options.unshift({
+      icon: <DashboardIcon />,
+      name: "Dashboard",
+      func: dashboard
+    })
+  }
+
+  const navigate = useNavigate();
+
+  function dashboard() {
+    navigate('/dashboard')
+  }
+
   return (
     <>
-
+      <SpeedDial
+        ariaLabel='SpeedDial tooltip example'
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        direction='down'
+        icon={<img
+          className='speedDialIcon'
+          src={user.avatar.url ? user.avatar.url : "/Profile.png"}
+          alt='Profile'
+        />
+        }
+      >
+        <SpeedDialAction icon={<DashboardIcon />} tooltipTitle="Dashboard" />
+      </SpeedDial>
     </>
   )
 }
